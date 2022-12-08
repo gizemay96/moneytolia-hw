669a9d25008d61ff3b490528b64c8efa9cbe4c2c
Gizem <gizem.ay1996@gmail.com>
2022-12-08 20:36:17 +0300

    v0.0.5

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,24 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { AuthServiceService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ]
 })
 
-export class LoginComponent {
+export default class LoginComponent {
   public loginForm: FormGroup;
   loginError = false;
 
   constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthServiceService
-    ) {
+  ) {
     this.loginForm = this.formBuilder.group({
       email: ["", [Validators.required]],
       password: ["", [Validators.required]]
     });
+  }
+
+  ngOnInit() {
+    this.loginForm.controls['email'].setValue('admin@gmail.com');
+    this.loginForm.controls['password'].setValue('12345');
+    this.authService.signOut();
   }
 
   login(): void {
@@ -28,8 +47,14 @@ export class LoginComponent {
         this.router.navigate(["/dashboard"]);
       } else {
         this.loginError = true;
+        // Object.keys(this.loginForm.value).map(key => { this.loginForm.controls[key].markAllAsTouched() });
       }
     }
+  }
+
+  setForm() {
+    this.loginForm.controls['email'].setValue('admin@gmail.com');
+    this.loginForm.controls['password'].setValue('12345');
   }
 
 
